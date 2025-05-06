5e2528d767f25f4868d5a44eb0bf31006c8c1285
Ubiquity <105232947+Ubiquity89@users.noreply.github.com>
2025-05-06 15:37:46 +0530

    Update vite.config.js

--- frontend-fresh/vite.config.js
+++ frontend-fresh/vite.config.js
@@ -14,7 +14,7 @@ export default {
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://codetracker-cxnp.onrender.com',
         changeOrigin: true,
         secure: false,
       },
